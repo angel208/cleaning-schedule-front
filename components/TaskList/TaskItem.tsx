c1ad08f4b2e5b0b79664b823501edee8eb780800
Angel Peña <angel.fp204@gmail.com>
2022-07-20 16:57:22 -0400

    reorder animation

--- components/TaskList/TaskItem.tsx
+++ components/TaskList/TaskItem.tsx
@@ -1,29 +1,85 @@
 
-import { Box, Checkbox, Flex, HStack, Spacer, VStack } from '@chakra-ui/react';
-import React from 'react'
+import { Box, Checkbox, Fade, Flex, HStack, IconButton, ScaleFade, Spacer, Spinner, VStack } from '@chakra-ui/react';
+import React, { useContext, useEffect, useState } from 'react'
 import { TaskDTO } from './TaskDTO'
 import { CgStopwatch, CgUndo, CgBrush, CgCheck } from 'react-icons/cg';
 import  getDateFormatted  from '../../utils/date.utils';
+import { useMutation } from 'react-query';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface TaskProps {
     task: TaskDTO;
+    updateCallBack: Function;
 }
 
-export default function TaskItem({ task } : TaskProps) {
-    return (
-        <Flex padding='6' boxShadow='lg' bg='white' w='full'>
-            <VStack  align={'left'}>
-                <Box fontSize={'xl'}>{task.name}</Box>
-                <HStack color={'gray.500'} spacing='16px'>
-                    <HStack fontSize={{ base: 'xs', md: 'sm' }} ><CgStopwatch /><Box>{task.duration_deep}m</Box></HStack>
-                    <HStack fontSize={{ base: 'xs', md: 'sm' }}><CgUndo /><Box>Every {task.frequency_deep} days</Box></HStack>
-                    <HStack fontSize={{ base: 'xs', md: 'sm' }}><CgBrush /><Box>{getDateFormatted(task.last_executed_deep as Date)}</Box></HStack>
-                </HStack>
-            </VStack>
-            
+const patchTaskAsDone = async (taskId: string) =>{
+    const baseUrl = 'http://127.0.0.1:3000/'
+    const endPoint = `task/${taskId}/done`
+    return fetch(baseUrl + endPoint, {
+        method: 'PATCH',
+    })
+}
 
-            <Spacer />
-            <Checkbox colorScheme='green' size='lg'></Checkbox>
-        </Flex>
+export default function TaskItem({ task, updateCallBack } : TaskProps) {
+    const [done, setDone] = useState(false)
+    const taskId = task._id;
+
+    const mutation = useMutation( () => {
+        return patchTaskAsDone(taskId)
+    })
+
+    const markTaskAsDone = () =>{
+        if(!done){
+            mutation.mutate( undefined , {
+                onSuccess: (data, variables, context) => {
+                    updateCallBack()
+                    setDone(true);
+                }
+            }) 
+            
+        }
+    }
+
+    return (
+       <>
+      
+        <motion.div animate={ done ? { x:[0, 75, 0] } : {}} style={{width: '100%'}} layout > 
+
+            <Flex padding='6' boxShadow='lg' bg='white' w='full'>
+                <VStack  align={'left'}>
+                    <Box fontSize={'xl'}>{task.name}</Box>
+                    <HStack color={'gray.500'} spacing='16px'>
+                        <HStack fontSize={{ base: 'xs', md: 'sm' }} ><CgStopwatch /><Box>{task.duration_deep}m</Box></HStack>
+                        <HStack fontSize={{ base: 'xs', md: 'sm' }}><CgUndo /><Box>Every {task.frequency_deep} days</Box></HStack>
+                        <HStack fontSize={{ base: 'xs', md: 'sm' }}><CgBrush /><Box>{getDateFormatted(task.last_executed_deep as Date)}</Box></HStack>
+                    </HStack>
+                </VStack>
+                
+                {mutation.isError ? (
+                        <div>An error occurred</div>
+                ) : null}
+                
+                
+                <Spacer />
+                {
+                    mutation.isLoading ? 
+                    <Box display={"flex"}><Spinner my={"auto"} /></Box> :
+                    <IconButton 
+                        aria-label='Search database' 
+                        size='md' 
+                        icon={<CgCheck size={24} />} 
+                        onClick={() => markTaskAsDone()}  
+                        variant={`${!done ? 'outline':'solid'}`} 
+                        colorScheme='green'
+                        isDisabled={done ? true:false}
+                        px={0}
+                    />
+                }
+
+            </Flex>
+
+        </motion.div>
+
+    </>
     )
 };
--- components/TaskList/TaskList.tsx
+++ components/TaskList/TaskList.tsx
@@ -6,15 +6,19 @@ import { TaskDTO } from './TaskDTO';
 import TaskItem from './TaskItem';
 import { Stack, VStack } from '@chakra-ui/react'
 
-const fetchTaskList = async () =>{
-    const data = await fetch('http://127.0.0.1:3000/cleaning-session')
+const fetchTaskList = async (availableTime: number) =>{
+    const baseUrl = 'http://127.0.0.1:3000/'
+    const endPoint = availableTime > 0 ?  `cleaning-session?availableTime=${availableTime}` : `cleaning-session`
+    const data = await fetch(baseUrl+endPoint)
     return data.json();
 }
 
 
 
-export default function TaskList() {
-    const {data: tasks, isLoading, error} = useQuery('tasks', () => fetchTaskList());
+export default function TaskList({ availableTime = 0 } : {availableTime:number}) {
+    const {data: tasks, isSuccess, isLoading, error, refetch} = useQuery('tasks', () => fetchTaskList(availableTime), {
+        refetchOnWindowFocus: false,
+    });
 
     if(isLoading)
         return <Loading></Loading>
@@ -22,13 +26,16 @@ export default function TaskList() {
     if(error)
         return <QueryError></QueryError>
 
-    return (
-        <VStack spacing='16px' w="full" mx="20px">
-            {
-                tasks.map( (task:TaskDTO) => 
-                    <TaskItem key={task._id} task={task} ></TaskItem>
-                )
-            }
-        </VStack>
-    )
+    if(isSuccess)
+        return (
+            <VStack spacing='16px' w="full" mx="20px">
+                {
+                    tasks.map( (task:TaskDTO) => 
+                        <TaskItem key={task._id} task={task} updateCallBack={refetch} ></TaskItem>
+                    )
+                }
+            </VStack>
+        )
+    
+    return <></>
 };
